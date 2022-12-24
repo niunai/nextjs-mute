ee48b0e47a6345150be469ba306d60e9fb9b40fa
niunai <shibah@gmail.com>
2022-12-24 23:29:23 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Alert, Box, Container, FormGroup, FormControlLabel, Switch } from "@mui/material";
+
+import setting from "../setting.json";
+
+const apiServerUrl = setting.apiServerUrl;
+
+export default function Home({ data }) {
+
+  const [isMute, setMute] = useState(JSON.parse(data.mute));
+  const [alertMsg, setAlertMsg] = useState("mute is " + data.mute);
+
+  const handleSwitchChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    if (evt.target.checked) {
+      fetch(apiServerUrl + "/mute")
+        .then((res) => res.json())
+        .then((data) => {
+          setMute(JSON.parse(data.mute));
+          setAlertMsg("set mute to " + data.mute);
+        })
+        .catch((err) => {
+          setAlertMsg(err.message);
+        });
+    } else {
+      fetch(apiServerUrl + "/unmute")
+        .then((res) => res.json())
+        .then((data) => {
+          setMute(JSON.parse(data.mute));
+          setAlertMsg("set mute to " + data.mute);
+        })
+        .catch((err) => {
+          setAlertMsg(err.message);
+        });
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 3,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <FormGroup>
+          <FormControlLabel
+            control={<Switch checked={isMute} onChange={handleSwitchChange} />}
+            label="Mute"
+          />
+        </FormGroup>
+        <Alert
+          style={{ width: "100%" }}
+          sx={{
+            marginTop: 3,
+          }}
+          severity="info">
+          {alertMsg}
+        </Alert>
+      </Box>
+    </Container>
+  );
+}
+
+export async function getServerSideProps() {
+
+  const res = await fetch(apiServerUrl)
+  const data = await res.json()
+
+  return { props: { data } }
+}
